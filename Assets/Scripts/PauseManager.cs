b3acdad7530666f09737e60e04190fc2caffdbc3
Lyse Lebourcq <lyse.lebourcq@ynov.com>
2024-01-22 11:18:56 +0100

    pause

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
